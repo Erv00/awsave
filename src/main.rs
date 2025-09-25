3ab8834d2b5f4185f7e959ebc6575b011c8c7533
Ervin Pektor <ervin@pektor.hu>
2025-09-25 12:06:44 +0200

    Multiupload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use std::{io, process::Stdio};
 
 use anyhow::Context;
@@ -18,12 +19,15 @@ use aws_sdk_s3::{
 use chacha20::cipher::{KeyIvInit, StreamCipher};
 use sha2::{Digest, Sha256};
 use tokio::io::AsyncBufReadExt;
+use tokio::sync::Mutex;
+use tokio::task::JoinSet;
 use tokio::{
     io::{AsyncRead, AsyncReadExt},
     process::Command,
 };
 
 const UPLOAD_CHUNK_SIZE: usize = 10 * 1024 * 1024; // 10 MiB
+const MAX_CONCURRENT: usize = 32;
 
 fn open_full(dataset: &str, snapshot: &str) -> Result<tokio::process::Child, io::Error> {
     Command::new("sudo")
@@ -70,14 +74,14 @@ where
 
 async fn abort_upload(
     client: &Client,
-    pc: &UploadConfig<'_>,
+    pc: &UploadConfig,
 ) -> Result<AbortMultipartUploadOutput, SdkError<AbortMultipartUploadError>> {
     println!("ABORTING UPLOAD");
     let r = client
         .abort_multipart_upload()
-        .bucket(pc.bucket)
-        .key(pc.key)
-        .upload_id(pc.id)
+        .bucket(&pc.bucket)
+        .key(&pc.key)
+        .upload_id(&pc.id)
         .send()
         .await;
     if r.is_err() {
@@ -88,8 +92,8 @@ async fn abort_upload(
 }
 
 async fn encypt_and_upload<R: AsyncReadExt + Unpin, C: StreamCipher>(
-    client: &Client,
-    pc: &UploadConfig<'_>,
+    client: Arc<Mutex<Client>>,
+    pc: UploadConfig,
     mut source: R,
     mut cipher: C,
 ) -> anyhow::Result<Vec<u8>> {
@@ -99,6 +103,8 @@ async fn encypt_and_upload<R: AsyncReadExt + Unpin, C: StreamCipher>(
     let mut upload_parts = Vec::new();
     let mut part_number = 1;
     let mut total = 0;
+
+    let mut running = JoinSet::new();
 
     loop {
         let r = read_exact_noerr(&mut source, &mut buffer).await;
@@ -114,29 +120,53 @@ async fn encypt_and_upload<R: AsyncReadExt + Unpin, C: StreamCipher>(
                 cipher.apply_keystream(&mut chunk);
                 hasher.update(&chunk);
 
-                match upload_part(client, pc, chunk, part_number).await {
-                    Ok(pr) => upload_parts.push(pr),
-                    Err(e) => {
-                        if let Err(ie) = abort_upload(client, pc).await {
-                            return Err(ie)
-                                .context(format!("Failed to upload part #{}", part_number))
-                                .context(e);
-                        } else {
-                            return Err(e.into());
-                        }
-                    }
-                }
                 total += n;
                 println!(
-                    "Uploaded chunk #{}, size was {}, total {} MiB",
+                    "Queued chunk #{}, size was {}, total {} MiB, running {}",
                     part_number,
                     n,
-                    total / 1024 / 1024
+                    total / 1024 / 1024,
+                    running.len()
                 );
+
+                running.spawn(upload_part_outer(
+                    client.clone(),
+                    pc.clone(),
+                    chunk,
+                    part_number,
+                ));
+
                 part_number = part_number + 1;
+
+                if running.len() > MAX_CONCURRENT {
+                    let res = running
+                        .join_next()
+                        .await
+                        .expect("More than max but still empty");
+
+                    match res {
+                        Ok(Ok(part)) => upload_parts.push(part),
+                        Ok(Err(e)) => {
+                            println!("ERROR, aborting");
+                            running.abort_all();
+                            let client = client.lock().await;
+                            if let Err(ie) = abort_upload(&client, &pc).await {
+                                return Err(ie)
+                                    .context(format!("Failed to upload part #{}", part_number))
+                                    .context(e);
+                            } else {
+                                return Err(e.into());
+                            }
+                        }
+                        Err(e) => todo!("Join error {}", e),
+                    }
+                }
             }
             Err(e) => {
-                if let Err(ie) = abort_upload(client, pc).await {
+                println!("ERROR, aborting");
+                running.abort_all();
+                let client = client.lock().await;
+                if let Err(ie) = abort_upload(&client, &pc).await {
                     return Err(ie)
                         .context(format!("Failed to read part #{}", part_number))
                         .context(e);
@@ -153,20 +183,41 @@ async fn encypt_and_upload<R: AsyncReadExt + Unpin, C: StreamCipher>(
         return Err(anyhow!("Read 0 bytes"));
     }
 
+    while let Some(jh) = running.join_next().await {
+        match jh {
+            Ok(Ok(part)) => upload_parts.push(part),
+            Ok(Err(e)) => {
+                println!("ERROR, aborting");
+                running.abort_all();
+                let client = client.lock().await;
+                if let Err(ie) = abort_upload(&client, &pc).await {
+                    return Err(ie)
+                        .context(format!("Failed to upload part #{}", part_number))
+                        .context(e);
+                } else {
+                    return Err(e.into());
+                }
+            }
+            Err(e) => todo!("Join error {}", e),
+        }
+    }
+
     let completed_multipart_upload = CompletedMultipartUpload::builder()
         .set_parts(Some(upload_parts))
         .build();
 
+    let client = client.lock().await;
+
     let cmu = client
         .complete_multipart_upload()
-        .bucket(pc.bucket)
-        .key(pc.key)
+        .bucket(&pc.bucket)
+        .key(&pc.key)
         .multipart_upload(completed_multipart_upload)
-        .upload_id(pc.id)
+        .upload_id(&pc.id)
         .send()
         .await;
     if let Err(e) = cmu {
-        if let Err(ie) = abort_upload(client, pc).await {
+        if let Err(ie) = abort_upload(&client, &pc).await {
             return Err(ie)
                 .context(format!("Failed to finalize upload"))
                 .context(e);
@@ -178,25 +229,35 @@ async fn encypt_and_upload<R: AsyncReadExt + Unpin, C: StreamCipher>(
     Ok(hasher.finalize().to_vec())
 }
 
-#[derive(Debug)]
-struct UploadConfig<'a> {
-    key: &'a str,
-    bucket: &'a str,
-    id: &'a str,
+#[derive(Debug, Clone)]
+struct UploadConfig {
+    key: String,
+    bucket: String,
+    id: String,
+}
+
+async fn upload_part_outer(
+    client: Arc<Mutex<Client>>,
+    pc: UploadConfig,
+    data: Vec<u8>,
+    part_number: i32,
+) -> Result<CompletedPart, SdkError<UploadPartError>> {
+    let c = client.lock().await;
+    upload_part(&c, &pc, data, part_number).await
 }
 
 async fn upload_part(
     client: &Client,
-    pc: &UploadConfig<'_>,
+    pc: &UploadConfig,
     data: Vec<u8>,
     part_number: i32,
 ) -> Result<CompletedPart, SdkError<UploadPartError>> {
     let data = ByteStream::from(data);
     let upload_part_res = client
         .upload_part()
-        .key(pc.key)
-        .bucket(pc.bucket)
-        .upload_id(pc.id)
+        .key(&pc.key)
+        .bucket(&pc.bucket)
+        .upload_id(&pc.id)
         .body(data)
         .part_number(part_number)
         .send()
@@ -247,9 +308,9 @@ async fn main() -> anyhow::Result<()> {
                 println!("{} ({:?})", b.key().unwrap_or("unknown"), b.storage_class());
 
                 let pc = UploadConfig {
-                    key: b.key().expect("Missing key"),
-                    bucket: "testbucket-paws",
-                    id: b.upload_id().expect("No upload id"),
+                    key: b.key().expect("Missing key").to_string(),
+                    bucket: "testbucket-paws".to_string(),
+                    id: b.upload_id().expect("No upload id").to_string(),
                 };
 
                 abort_upload(&client, &pc).await?;
@@ -269,25 +330,27 @@ async fn main() -> anyhow::Result<()> {
     let o = tokio::io::BufReader::new(o);
 
     let mut pc = UploadConfig {
-        key: "testdata_T2",
-        bucket: "testbucket-paws",
-        id: "asd001",
+        key: "testdata_T2".to_string(),
+        bucket: "testbucket-paws".to_string(),
+        id: "asd001".to_string(),
     };
 
     // Open upload
     let multipart_upload_res: CreateMultipartUploadOutput = client
         .create_multipart_upload()
-        .bucket(pc.bucket)
-        .key(pc.key)
+        .bucket(&pc.bucket)
+        .key(&pc.key)
         .send()
         .await?;
 
     let upload_id = multipart_upload_res
         .upload_id()
         .ok_or(anyhow!("Missing upload_id after CreateMultipartUpload"))?;
-    pc.id = upload_id;
+    pc.id = upload_id.to_string();
 
-    match encypt_and_upload(&client, &pc, o, cipher).await {
+    let cc = Arc::new(Mutex::new(client));
+
+    match encypt_and_upload(cc.clone(), pc, o, cipher).await {
         Ok(hash) => println!("Upload done, hash is {}", hex::encode(hash)),
         Err(e) => {
             println!("Cought error {}", e);
